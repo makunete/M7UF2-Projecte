db72c5833de2ea68915afe619b0064ba1f8f8de8
Marc Sola <mmarcsola@gmail.com>
2017-02-10 20:27:40 +0100

    Rutas nuevas

--- laravelapp2/routes/web.php
+++ laravelapp2/routes/web.php
@@ -12,5 +12,36 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+//Crear Categoria
+Route::get('/categories/crea', "CategoriesController@create");
+
+Route::post('/categories/crea', "CategoriesController@store");
+//Mostrar Categorias
+Route::get('/categories', "CategoriesController@index");
+//Mostrar Categoria concreta
+Route::get('/categories/{categories}', "CategoriesController@show");
+
+//Crear Videojuego
+Route::get('/videojoc/{videojoc}/crea', "VideojocsController@create");
+
+Route::post('/videojoc/{videojoc}/crea', "VideojocsController@store");
+
+//Mostrar Videojoc concret
+Route::get('/videojoc/{videojoc}', "VideojocController@show");
+
+//Esborrar videojoc
+Route::get('/videojoc/{videojoc}', "VideojocController@delete");
+
+
+// Crea una nova venda 
+Route::get('/vendes/crea', "VendesController@create");
+Route::post('/vendes/crea', "VendesController@store");
+//Mostrar vendes general
+Route::get('/vendes', "VendesController@index");
+// Mostra les vendes d'un videojoc
+Route::get('/vendes/{vendes}', "VendesController@show");
+// administra vendes
+Route::get('/vendes/{vendes}/admin', "VendesController@admin");
+
