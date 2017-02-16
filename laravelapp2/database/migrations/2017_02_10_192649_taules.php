898650a9d3d1657c089876708c6cebeafd920208
Marc Sola <mmarcsola@gmail.com>
2017-02-16 18:35:29 +0100

    database

--- laravelapp2/database/migrations/2017_02_10_192649_taules.php
+++ laravelapp2/database/migrations/2017_02_10_192649_taules.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/*use App\Categoria;
+use App\Videojuego;
+use App\Venta;
+*/
 class Taules extends Migration
 {
     /**
@@ -14,6 +18,47 @@ class Taules extends Migration
     public function up()
     {
         //
+        Schema::create('categorias', function(Blueprint $table) {
+            $table->increments("id");
+            $table->string("nombre");
+            $table->string("videojuegos");
+            $table->timestamps();
+        });
+
+        Schema::create('videojuegos', function(Blueprint $table) {
+            $table->increments("id");
+            $table->string("nombre");
+            $table->float("precio");
+            $table->string("categorias");
+            $table->timestamps();
+        });
+/*
+        Schema::create('ventas', function(Blueprint $table) {
+            $table->increments("id");
+            $table->string("videojuego");
+            $table->integer("num_ventas");
+            $table->timestamps();
+        });
+*/
+        Schema::create('categorias_videojuegos', function(Blueprint $table) {
+            $table->increments("id");
+            $table->integer("id_categoria");
+            $table->integer("id_videojuego");
+
+        });
+
+/*        Schema::table('categorias_videojuegos',function(Blueprint $table) {
+            $table->foreign("id_categoria")->references('id')->on('categorias')
+                    ->onUpdate('cascade')->onDelete('cascade');
+        });
+*/
+            Schema::table('categorias_videojuegos',function(Blueprint $table) {
+            $table->foreign("id_videojuego")->references('id')->on('videojuegos')
+                    ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+
+
     }
 
     /**
@@ -24,5 +69,9 @@ class Taules extends Migration
     public function down()
     {
         //
+        Schema::drop("categorias");
+        Schema::drop("videojuegos");
+        Schema::drop("ventas");
+        Schema::drop("categorias_videojuegos");
     }
 }
